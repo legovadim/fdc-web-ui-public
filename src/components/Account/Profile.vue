6c4f43ec5b6cade0803578a6ca7f613cfd6454a2
smartmaxdev <smartmaxdev@gmail.com>
2018-10-10 12:36:40 -0400

    WEBUI-21 Implement Profile screen

--- src/components/Account/Profile.vue
+++ src/components/Account/Profile.vue
@@ -1,22 +1,79 @@
 <template>
-  <div class="row">
+  <div class="vld-parent">
+    <!-- user profile -->
     
+        <loading :active.sync="isLoading" 
+        :can-cancel="true" 
+        :on-cancel="onCancel"
+        :is-full-page="fullPage"></loading>
+        
     <div class="row">
-      <div class="col-lg-12 col-sm-8">
-      <h3>This is the Profile page</h3>
-      <div v-if="signedIn">
-      <h4>username {{this.account.username}}</h4>
-      <h4>email {{this.account.attributes.email}}</h4>
+      <div class="col-lg-4 col-sm-3">
+        <form>
+            <div class="form-group">
+              <label>Email address</label>
+              <fg-input  type="email" name="email" v-validate="modelValidations.email" :error="getError('email')" v-model="userData.email"></fg-input>
+            </div>
+            <div class="form-group">
+              <label>First Name</label>
+              <fg-input  type="firstname" name="firstname" v-validate="modelValidations.firstname" :error="getError('firstname')" v-model="userData.firstname"></fg-input>
+            </div>
+            <div class="form-group">
+              <label>Last Name</label>
+              <fg-input  type="lastname" name="lastname" v-validate="modelValidations.lastname" :error="getError('lastname')" v-model="userData.lastname"></fg-input>
+            </div>
+        </form>
+      </div>
     </div>
+    <!-- user market place table -->
+    <div class="row">
+      <div class="col-lg-12 card1 table-responsive">
+        <h4>Which marketplaces (Upwork, Golance, Fiverr, etc.) do you use?</h4>
+        <table cellspacing="0" cellpadding="0" border="0" class="el-table ">
+          <thead class="has-gutter">
+            <tr><th v-for="column in columns" :key="column.id">{{column}}</th></tr>
+          </thead>
+          <tbody>
+            <tr v-for="(marketplace,index) in userData.marketplaces"  :key="marketplace.id">
+              <td>{{marketplace.name}}</td>
+              <td>{{marketplace.profile_url}}</td>
+              <td style="width: 18%; height:50px;"><a href="#!" @click="deleete(index)"><i class="fa fa-times"></i></a></td>
+            </tr>
+            <tr>
+              <td>
+                <div class="input-field">
+                  <input class="form-control" placeholder="Name" ref="name" v-model="input.name" id="name" type="text">
+                </div>
+              </td>
+              <td>
+                <div class="input-field">
+                  <input class="form-control"  placeholder="Profile URL" v-model="input.profile_url" id="profile_url" type="text">
+                </div>
+              </td>
+              <td><a href="#!" @click="add" class="btn btn-waves">add</a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+<!-- // submit button -->
+    <div class="row">
+      <a href="#!" @click="updateProfile" class="btn btn-success">Save</a>
     </div>
   </div>
 </template>
 <script>
 import { AmplifyEventBus } from "aws-amplify-vue";
 import { Auth } from "aws-amplify";
+import axios from 'axios';
+
+import Loading from 'vue-loading-overlay';
+
+import 'vue-loading-overlay/dist/vue-loading.css';
+
 export default {
-  components: {},
+  components: {Loading},
+
   async beforeCreate() {
     try {
       this.account = await Auth.currentAuthenticatedUser();
@@ -33,13 +90,129 @@ export default {
       }
     });
   },
+
+  mounted () {
+
+    this.getProfile()
+  },
+
   data() {
     return {
-      signedIn: false,
-      account: {}
+      isLoading: false,
+      fullPage: true,
+      userData: {
+        "entity_id": "",
+        "version": "",
+        "changed_by_id": "",
+        "user_id": "",
+        "changed_on": "",
+        "firstname": "",
+        "lastname": "",
+        "email": "",
+        "marketplaces": []
+      },
+      
+      modelValidations: {
+        email: {
+          required: true,
+          email: true
+        },
+        firstname: {
+          required: true,
+          min: 3
+        },
+        lastname: {
+          required: true,
+          min: 3
+        }
+      },
+
+      columns: ["Marketplace", "Profile URL", ""],
+      
+      input: {
+        name: "",
+        profile_url: ""
+      },
+      editInput: {
+        name: "",
+        profile_url: ""
+      }
     };
+  },
+  
+  methods: {
+
+    getProfile() {
+      this.isLoading = true;
+      axios
+      .get(process.env.VUE_APP_ROOT_API+'/users/'+'80586340-5b00-419b-8b45-9875e96770fd')//this should change later to username
+      .then(response => {
+        console.log(response.data)
+        this.userData = response.data
+        this.isLoading = false;
+      })
+      .catch(error => {
+        console.log(error)
+        
+      })
+      .finally(() => this.isLoading = false)
+    },
+    updateProfile() {
+      this.isLoading = true;
+      axios
+      .put(process.env.VUE_APP_ROOT_API+'/users', JSON.stringify(this.userData),{
+        headers: {
+            'Content-Type': 'application/json',
+        }
+      })//this should change later to username
+      .then(response => {
+        console.log(response.data)
+        this.getProfile()
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      .finally(() => this.isLoading = false)
+    },
+
+    getError(fieldName) {
+      return this.errors.first(fieldName);
+    },
+
+    validate() {
+      this.$validator.validateAll().then(isValid => {
+        this.$emit("on-submit", this.registerForm, isValid);
+      });
+    },
+
+    add: function() {
+      this.userData.marketplaces.push({
+        name: this.input.name,
+        profile_url: this.input.profile_url
+      });
+
+      for (var key in this.input) {
+        this.input[key] = "";
+      }
+      this.$refs.name.focus();
+
+    },
+    //function to defintely delete data
+    deleete: function(index) {
+      this.userData.marketplaces.splice(index, 1);
+    }
   }
 };
 </script>
-<style>
+<style lang="scss">
+.el-table .td-actions {
+  button.btn {
+    margin-right: 5px;
+  }
+}
+
+.card1 {
+    background-color: #FFFFFF;
+    
+}
 </style>
